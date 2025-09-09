c8855cd39cc51bba6dc09d5ade1c2ff7e054be5b
Igor Gorbanev <gorbanev_i@mail.ru>
2025-09-09 17:21:16 +0300

    create Todolist action

--- src/model/todolist-reducer.test.ts
+++ src/model/todolist-reducer.test.ts
@@ -1,7 +1,7 @@
 import {v1} from 'uuid'
 import {expect, test} from 'vitest'
 import type {Todolist} from '../App.tsx'
-import {deleteTodolistActionCreator, todolistsReducer} from "./todolost-reducer.ts";
+import {deleteTodolistAC, createTodolistAC, DeleteTodolistAction, todolistsReducer} from "./todolost-reducer.ts";
 
 test ("correct todolist should be deleted", ()=>{
     const todolistId1=v1()
@@ -12,7 +12,7 @@ test ("correct todolist should be deleted", ()=>{
         {id: todolistId2, title: 'what to buy', filter: 'all'}
     ]
 
-    const action = {
+    const action:DeleteTodolistAction = {
         type: 'delete_todolist',
         payload: {
             id: todolistId1,
@@ -21,4 +21,18 @@ test ("correct todolist should be deleted", ()=>{
     const endState=todolistsReducer(startState, deleteTodolistAC(todolistId1))
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolistId2)
+})
+
+test ("correct todolist should be created", ()=>{
+    const todolistId1=v1()
+    const todolistId2=v1()
+
+    const startState: Todolist[]=[
+        {id: todolistId1, title: 'what to learn', filter: 'all'},
+        {id: todolistId2, title: 'what to buy', filter: 'all'}
+    ]
+    const title = 'New todolist'
+    const endState=todolistsReducer(startState, createTodolistAC(title))
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(title)
 })
--- src/model/todolost-reducer.ts
+++ src/model/todolost-reducer.ts
@@ -1,21 +1,33 @@
 import {Todolist} from "../App.tsx";
+import {v1} from 'uuid'
 
-type Actions={
+/*export type DeleteTodolistAction={
     type: 'delete_todolist'
     payload: {
-        id: string
+        id: string,
+        title?: string
     }
-}
+}*/
 
 export const deleteTodolistAC=(id: string): DeleteTodolistAction =>{
     return {type: 'delete_todolist', payload: {id}}as const
 }
+export const createTodolistAC=(title: string): CreateTodolistAction =>{
+    return {type: 'create_todolist', payload: {id:v1(), title}}as const
+}
+export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
+export type CreateTodolistAction = ReturnType<typeof createTodolistAC>
+
 
 const initialState: Todolist[]=[]
 export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
     switch (action.type) {
         case 'delete_todolist': {
             return state.filter(todolist => todolist.id !== action.payload.id)
+        }
+        case 'create_todolist': {
+            const newTodolist: Todolist = {id: action.payload.id, title: action.payload.title, filter: 'all'}
+            return [...state, newTodolist]
         }
         default:
             return state
